f83209b050e0dd7c22f887b8bf71d59fa64d83cd
Nayden Luiz dos Santos Cruz <nayden.luiz@gmail.com>
2020-03-27 21:57:13 -0300

    initial commit with code

---
+++ MyClasses/MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            Assert.Inconclusive();
+
+        }
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall = fp.FileExists(@"C:\Windows\BadFileName.bad");
+            Assert.IsTrue(fromCall);
+           
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        
+        public void FileNameNullOrEmpty_ThrowsArgumentNullExceptionUsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException e)
+            {
+                //The Test was a success
+                return;
+            }
+            Assert.Fail("Call to FIleExists did Not Throw an ArgumentNullException ");
+        }
+    }
+}
